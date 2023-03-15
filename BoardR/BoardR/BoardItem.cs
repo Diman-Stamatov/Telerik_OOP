30924d58dba95fae869c54247fb4b1719126b5fb
D1manko <d.stamatov@yahoo.com>
2023-03-15 22:43:16 +0200

    Added BoardItem properties, constructor validation and methods

--- BoardR/BoardR/BoardItem.cs
+++ BoardR/BoardR/BoardItem.cs
@@ -3,14 +3,104 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoardR;
 
 namespace BoardR
 {
+
     internal class BoardItem
     {
         private string title;
         private DateTime dueDate;
-        private enum status;
+        private ItemStatus status;
 
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            private set
+            {
+                if (value == null || TitleIsValid(value) == false)
+                {
+                    throw new ArgumentException("Please specify a title that is between 5 and 30 characters long!");
+                }
+
+                this.title = value;
+            }
+        }
+        public DateTime DueDate
+        {
+            get
+            {
+                return this.dueDate;
+            }
+            private set
+            {
+                var creationDate = DateTime.Now;
+                if (creationDate > value)
+                {
+                    throw new ArgumentException("The due date cannot be in the past!");
+                }
+                this.dueDate = value;
+            }
+        }
+        public ItemStatus Status
+            {
+                 get 
+                 { 
+                     return this.status;
+                 }
+            }
+
+        public BoardItem(string title, DateTime dueDate)
+        {
+            this.Title = title;
+            this.DueDate = dueDate;
+            this.status = 0;
+        }
+        private bool TitleIsValid(string value)
+        {
+
+            bool titleIsValid = true;
+            if (value == null)
+            {
+                titleIsValid = false;
+            }
+            int newTitleLength = value.Length;
+            int minimumTitleLength = 5;
+            int maximumTitleLength = 30;
+            if (newTitleLength < minimumTitleLength || newTitleLength > maximumTitleLength)
+            {
+                titleIsValid = false;
+            }
+            return titleIsValid;
+
+
+
+
+        }
+        public void RevertStatus()
+        {
+            if (this.status !=0)
+            {
+                this.status--;
+            }
+        }
+        public void AdvanceStatus()
+        {
+            int maxStatusIndex = Enum.GetNames(typeof(ItemStatus)).Length -1;
+            if ((int)this.status < maxStatusIndex)
+            {
+                this.status++;
+            }
+        }
+
+        public string ViewInfo()
+        {
+            return $"'{title}', [{status}|{dueDate.ToString("dd-MM-yyyy")}]";
+        }
+        
     }
 }
--- BoardR/BoardR/Program.cs
+++ BoardR/BoardR/Program.cs
@@ -2,9 +2,56 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                var item = new BoardItem("Refactor this mess", DateTime.Now.AddDays(2));
+                Console.WriteLine(item.Title); // Refactor this mess
+                Console.WriteLine(item.DueDate); // 25/01/2020 12:09:49 PM (this will vary depending on when you run the code)
+                Console.WriteLine(item.Status); // Open            
+                item.AdvanceStatus();
+                Console.WriteLine(item.Status); // Todo
+                item.AdvanceStatus();
+                Console.WriteLine(item.Status); // InProgress
+                item.RevertStatus();
+                Console.WriteLine(item.Status); // Todo
+                Console.WriteLine(item.ViewInfo()); // 'Refactor this mess', [Open|25-01-2020]
+
+                item = new BoardItem("Refactor this mess", DateTime.Now.AddDays(2));
+                item.AdvanceStatus();
+                var anotherItem = new BoardItem("Encrypt user data", DateTime.Now.AddDays(10));
+
+                Board.items.Add(item);
+                Board.items.Add(anotherItem);
+
+                foreach (var boardItem in Board.items)
+                {
+                    boardItem.AdvanceStatus();
+                }
+
+                foreach (var boardItem in Board.items)
+                {
+                    Console.WriteLine(boardItem.ViewInfo());
+                }
+
+                // Output:
+                // 'Refactor this mess', [InProgress|25-01-2020]
+                // 'Encrypt user data', [Todo|02-02-2020]
+
+            }
+            catch (Exception exception)
+            {
+
+                Console.WriteLine(exception.Message);
+            }
+            
+
+
+
+
+
         }
     }
 }
