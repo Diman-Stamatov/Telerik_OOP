88e55191eab2446523a425ecbd909425904b94d7
Diman_Stamatov <d.stamatov@yahoo.com>
2023-03-15 16:15:40 +0200

    Adding initial classes.

---
+++ BoardR/BoardR/Board.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardR
+{
+    internal class Class1
+    {
+    }
+}
