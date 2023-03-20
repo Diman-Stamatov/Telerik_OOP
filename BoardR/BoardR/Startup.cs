99fcc12229477c093a7aed2681bc1ff73ff0317f
D1manko <d.stamatov@yahoo.com>
2023-03-20 23:46:01 +0200

    Updated the Startup.

--- BoardR/BoardR/Startup.cs
+++ BoardR/BoardR/Startup.cs
@@ -44,13 +44,7 @@ namespace BoardR
             issue.AdvanceStatus();
             issue.DueDate = issue.DueDate.AddDays(1);
             Console.WriteLine(issue.ViewHistory());*/
-            var tomorrow = DateTime.Now.AddDays(1);
-            var issue = new Issue("App flow tests?", "We need to test the App!", tomorrow);
-            var task = new Task("Test the application flow", "Peter", tomorrow);
-
-            Board.AddItem(issue);
-            Board.AddItem(task);
-            Console.WriteLine(Board.TotalItems); // 2
+            
 
         }
     }
