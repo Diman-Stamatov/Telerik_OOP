8caed4b89b7f40e8f143bbbd2c82550fc5eb2ca0
D1manko <d.stamatov@yahoo.com>
2023-04-05 01:25:35 +0300

    Removed unused Validation method for Decimal

--- OOP Workshop 4 - Car Dealership/Dealership/Validator.cs
+++ OOP Workshop 4 - Car Dealership/Dealership/Validator.cs
@@ -29,14 +29,6 @@ namespace Dealership
             }
         }
 
-        public static void ValidateDecimalRange(decimal value, decimal min, decimal max, string message)
-        {
-            if (value < min || value > max)
-            {
-                throw new ArgumentException(message);
-            }
-        }
-
         public static void ValidateSymbols(string value, string pattern, string message)
         {
             var regex = new Regex(pattern, RegexOptions.IgnoreCase);
